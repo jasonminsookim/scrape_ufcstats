ffc23829d9524bae53492c6d01653a777ef5ade4
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2022-06-14 23:38:12 +0000

    Change  column types to datetime rather than str

--- src/data/io.py
+++ src/data/io.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 engine = create_engine(os.getenv("POSTGRES_DB_URL"))
 events_df = pd.read_csv("../data/events.csv")
+events_df["event_date"] = pd.to_datetime(events_df["event_date"])
+events_df["datetime_scraped"] = pd.to_datetime(events_df["datetime_scraped"])
 print("Events df shape:")
 print(events_df.shape)
 events_df.to_sql("events", engine, if_exists="replace", index=False)
